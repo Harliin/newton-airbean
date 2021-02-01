8a6b03067611594e880b7efb7064102ee3abf995
Harliin <56713048+Harliin@users.noreply.github.com>
2021-02-01 15:01:12 +0100

    wip

---
+++ airbean/src/views/Landing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Landing" @click="toMenu">
+    <div class="left"></div>
+    <div class="middle"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods:{
+    toMenu(){
+      this.$router.push('/menu')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Landing{
+  width: 100%;
+  height: 100%;
+  background-color: #38846D;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-image: url(~@/assets/intro-graphic-left.svg), url(~@/assets/airbean-landing.svg),  url(~@/assets/intro-graphic-right.svg);
+  background-position: left center, center center, right center;
+  background-repeat: no-repeat, no-repeat, no-repeat;
+  background-size: 26vh, auto, 26vh ;
+}
+
+
+</style>
---
+++ airbean/src/views/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Menu">
+      <h2>iahndi
+          
+      </h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.Menu{
+    background-color: #F3E4E1;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(~@/assets/graphics-header.svg), url(~@/assets/graphics-footer.svg);
+    background-position: top center, bottom center;
+    background-repeat: no-repeat, no-repeat;
+    background-size: 100vw, 100vw;
+}
+</style>
