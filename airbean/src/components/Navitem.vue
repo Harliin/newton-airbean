46e1bc155ca50fc6d14fc4a5f4821503126a9f9c
sadqw fwe <fmolen@gmail.com>
2021-02-01 15:53:42 +0100

    about klar

---
+++ airbean/src/components/Navitem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="navitem">
+        <button class="button"><img src="~@/assets/navicon.svg" alt="hitta ej"></button>
+        <div class="menu"></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    width: 45px;
+    height: 45px;
+    border-radius: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.navitem {
+    position: absolute;
+    margin: 20px 0px 0px 15px;
+}
+</style>
--- airbean/src/views/About.vue
+++ airbean/src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+      <Navitem class="navitem"/>
+      <div class="text">
+      <h1>Vårt kaffe</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse repellendus fugiat nisi? Adipisci atque laudantium delectus amet, eius praesentium doloremque totam veniam deleniti rerum laborum aperiam neque temporibus natus possimus!</p>
+      </div>
+      <div class="eva-cortado">
+       <img src="~@/assets/eva-cortado.jpg">
+       <h3>Eva Cortado</h3>
+       <p>VD & Grundare</p>
+      </div>
+    </div>
 </template>
+
+
+<script>
+import Navitem from '@/components/Navitem'
+
+export default {
+  components: {Navitem}
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  background-color: #F3E4E1;
+  width: 100%;
+  height: 150%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-image: url(~@/assets/graphics-header.svg), url(~@/assets/graphics-footer.svg);
+  background-position: top center, bottom center;
+  background-repeat: no-repeat, no-repeat;
+  background-size: 100vw, 100vw;
+}
+
+.text {
+  margin-top: 10rem;
+}
+
+.navitem {
+  align-self: flex-start;
+}
+
+</style>
