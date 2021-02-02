c4808f1007b03ce509e2fb23c8a775e86da31fbe
sadqw fwe <fmolen@gmail.com>
2021-02-02 17:27:19 +0100

    profile-sida klar, behövs formateras lite

---
+++ airbean/src/components/Signup.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="signup" v-if="showSignup"> 
+    <div class="logo">
+      <img src="~@/assets/Vector.svg" alt="hej">
+    </div>
+      <h3>Välkommen till AirBean-familjen!</h3>
+      <p>Genom att skapa ett konto nedan kan du spara och se din orderhistorik</p>
+
+      <form @submit.prevent="doSignup">
+        <label for="namn">Namn</label><br>
+        <input type="text" v-model="namn" name="namn"><br>
+        <label for="epost">Epost</label><br>
+        <input type="text" v-model="epost" name="epost"><br>
+        <div class="green-circle">
+          <p>GDPR Ok!</p>
+        </div>
+        <br>
+        <br>
+        <input type="submit" value="Brew me a cup!" class="submit">
+      </form> 
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['showSignup'],
+  data() { return {
+          namn: '',
+          epost: ''
+  }},
+  
+  computed: {
+  },
+  methods: {
+    doSignup() {
+      if (this.namn == null) {
+        alert('Måste skriva ett namn')
+        return 
+      }
+      if (this.epost == null) {
+        alert('Måste ha en epost')
+        return
+      }
+      localStorage.setItem('namn', this.namn)
+      localStorage.setItem('epost', this.epost)
+      this.$emit('signupDone')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  color: black;
+}
+.signup {
+    height: 85%;
+    width: 90%;
+    background-color: #F3E4E1;
+    border-radius: 5px;
+    position: relative;
+    top: 5rem;
+    display: flex;
+    flex-direction: column;
+
+    >h3 {
+      left: 8.27%;
+      right: 9.33%;
+      top: 20.84%;
+      bottom: 64.62%;
+
+      font-family: PT Serif;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 32px;
+      line-height: 120%;
+
+      /* or 38px */
+      display: flex;
+      align-items: center;
+      text-align: center;
+
+      /* Airbean - brown */
+      color: #2F2926;
+    }
+    >p {
+      width: 305px;
+      height: 56px;
+     align-self: center;
+
+      font-family: Work Sans;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 16px;
+      line-height: 150%;
+
+      /* Airbean - brown */
+      color: #2F2926;
+    }
+}
+.logo {
+  margin-top: 2rem;
+  align-self: center;
+  height: 30px;
+  width: 30px;
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  border-radius: 50px;
+  justify-content: center;
+  img {
+    align-self: center;
+    justify-self: center;
+    width: 20px;
+    height: 20px;
+  }
+}
+
+form {
+  margin-top: 20%;
+}
+
+label {
+  font-size: 10px;
+  margin-left: -80%;
+}
+
+input {
+  background-color:  rgba(0, 0, 0, 0);
+  height: 40px;
+  width: 90%;
+  /* Airbean - brown */
+  border: 1px solid #2F2926;
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 20px;
+}
+
+.green-circle {
+  border-radius: 50px;
+  height: 10px;
+  width: 10px;
+  background-color: green;
+  border-style: solid;
+  border-width: 1px;
+  margin-left: 5%;
+  margin-top: 5%; 
+
+  >p {
+    font-size: 10px;
+    margin-left: 15px;
+    width: 80px;
+
+    font-family: Work Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 120%;
+
+    /* or 14px */
+    display: flex;
+    align-items: center;
+
+    /* Airbean - brown */
+    color: #2F2926;
+  }
+
+}
+
+.submit {
+  width: 200px;
+  height: 50px;
+  border-radius: 30px;
+  background-color: #2F2926;
+  border: none;
+  font-size: 21px;
+  font-weight: 700;
+  color: white;
+  padding-top: 10px;
+}
+</style>
--- airbean/src/views/Profile.vue
+++ airbean/src/views/Profile.vue
@@ -1,19 +1,112 @@
 <template>
   <div class="profile">
       <Navitem class="nav-item"/>
+      <Signup :showSignup="showSignup" v-on:signupDone="signupDone()" />
+      <div class="profile-page" v-if="!showSignup">
+        <div class="logo">
+          <div class="head"></div>
+          <div class="body"></div>
+        </div>
+
+        <h3>{{namn}}</h3>
+        <p>{{epost}}</p>
+
+        <div class="order-list">
+          <h3>Orderhistorik</h3>
+          
+          <div v-bind:key="profile.orderNo" v-for="profile in profileData">
+            <div class="bar">
+              <p>{{profile.orderNo}}<br>Total ordersumma</p><br>
+              <p>{{profile.orderDate}}<br>{{profile.orderSum}} kr</p>
+            </div>
+            <hr>
+          </div>
+          <hr>
+          <p>Totalt spenderat {{totalSpent}} kr</p>
+        </div>
+      </div>
+
   </div>
 </template>
 
 <script>
 import Navitem from '@/components/Navitem'
+import Signup from '@/components/Signup'
+
 export default {
-    components:{ Navitem}
+    components:{ Navitem, Signup },
+    computed: {
+      namn() {
+        return localStorage.getItem('namn')
+      },
+      epost() {
+        return localStorage.getItem('epost')
+      },
+      totalSpent() {
+        let total = 0
+        for (let i = 0; i < this.profileData.length; i++) {
+          total += this.profileData[i].orderSum
+        }
+        return total
+      }
+    },
+    created: function () {
+      this.getSignupStatus()
+    },
+    updated: function () {
+      this.getSignupStatus()
+    },
+    data(){ return {
+      showSignup: true,
+        profileData: [
+          {
+          orderNo: '#ANP4342342F',
+          orderDate: '20/03/03',
+          orderSum: 323
+          },
+          {
+          orderNo: '#FKN343225F',
+          orderDate: '20/03/03',
+          orderSum: 533
+          },
+          {
+          orderNo: '#EF4324234G',
+          orderDate: '20/03/04',
+          orderSum: 234
+          }
+        ]
+    }},
+    methods: {
+      signupDone() {
+        this.showSignup = false
+        location.reload()
+      },
+      getSignupStatus() {
+        if (localStorage.getItem('namn') == '') {
+        this.showSignup = true
+        return
+      }
+      if (localStorage.getItem('epost') == '') {
+        this.showSignup = true
+        return
+      }
+      if (localStorage.getItem('namn') == undefined){
+        this.showSignup = true
+        return
+      }
+      if (localStorage.getItem('epost') == undefined) {
+        this.showSignup = true
+        return
+      }
+      this.showSignup = false
+      }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .profile{
-    background-color: #2F2926;
+  background-color: #2F2926;
   width: 100%;
   height: 100%;
   display: flex;
@@ -27,5 +120,58 @@ export default {
 }
 .nav-item{
     align-self: flex-start;
+}
+.logo {
+  background-color: #F3E4E1;
+  border-radius: 50px;
+  height: 100px;
+  width: 100px;
+  margin-top: 8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  margin-inline: auto;
+  .head {
+    margin-top: 10px;
+    background-color: #E5674E;
+    height: 120px;
+    width: 50px;
+    border-radius: 50px;
+
+  }
+  .body {
+    background-color: #E5674E;
+    height: 100px;
+    width: 80px;
+    border-radius: 50px;
+    
+  }
+  .profile-page {
+    color: #FFFFFF;
+  } 
+}
+
+.order-list {
+  display: flex;
+  flex-direction: column;
+  color: white;
+  >h3 {
+    justify-self: flex-start;
+    margin-top: 5rem;
+  }
+}
+
+h3 {
+    color: white;
+}
+p {
+  color: white;
+}
+.bar {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 }
 </style>
