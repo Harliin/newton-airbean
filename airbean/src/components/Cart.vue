97e296f4c705990458ac718b4aaedaaf1b6d86b4
Harliin <56713048+Harliin@users.noreply.github.com>
2021-02-04 10:18:20 +0100

    wip

--- airbean/src/components/Cart.vue
+++ airbean/src/components/Cart.vue
@@ -7,17 +7,23 @@
     </button>
     <section v-if="cartOpen" class="cart-container">
         <span class="arrow"></span>
-        <h2>Din beställning</h2>
-        <div><CartItem v-for="item in cart" :key="item.id" :cart="item"/></div>
-        <div class="total-container">
-            <div class="total">
-                <h3>Total</h3>
-                <span></span>
-                <h3>{{Total}} kr</h3>
+        <div v-if="!loading" class="order-container">
+            <h2>Din beställning</h2>
+            <div class="cart-items"><CartItem v-for="item in cart" :key="item.id" :cart="item"/></div>
+            <div class="total-container">
+                <div class="total">
+                    <h3>Total</h3>
+                    <span></span>
+                    <h3>{{Total}} kr</h3>
+                </div>
+                <p>inkl moms + drönarleverans</p>
             </div>
-            <p>inkl moms + drönarleverans</p>
+            <button @click="sendOrder">Take my money!</button>
         </div>
-        <button @click="sendOrder">Take my money!</button>
+        <div class="loader" v-if="loading">
+            <img src="~@/assets/loader.png" alt="kunde inte hitta">
+        </div>
+        
     </section>
   </div>
 </template>
@@ -27,7 +33,8 @@ import CartItem from '@/components/CartItem'
 export default {
     components: {CartItem},
     data(){return{
-        cartOpen: false
+        cartOpen: false,
+        loading: false
     }},
     computed: {
         cart() {
@@ -47,7 +54,9 @@ export default {
     },
     methods:{
         async sendOrder(){
+            this.loader = true
             await this.$store.dispatch('order')
+            this.loader = false
             this.$router.push('/status')
         }
     }
@@ -105,66 +114,98 @@ export default {
         box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
         border-radius: 3px;
         margin-top: 17%;
-        display: flex;
-        justify-content: space-around;
-        flex-direction: column;
-        align-items: center;
+        
 
-        .arrow{
-            width: 0;
-            height: 0;
-            border-left: 14px solid transparent;
-            border-right: 14px solid transparent;
-            border-bottom: 20px solid white;
-            position: absolute;
-            right: 3%;
-            top: -2%;
-        }
+        .order-container{
+            height: 80vh;
+            width: 90vw;
+            background-color: #FFFFFF;
+            display: grid;
+            align-items: center;
+            place-items: center;
+            grid-template-rows: 15% 55% 10% 20%;
 
-        h2{
-            font-size: 32px;
-            font-weight: bold;
-            color: #2F2926;
-            line-height: 120%;
-        }
-        .total-container{
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: flex-start;
-            margin-top: 7rem;
-            color: #2F2926;
-            .total{
+        
+            .cart-items{
+                place-self: flex-start;
+            }
+
+            .arrow{
+                width: 0;
+                height: 0;
+                border-left: 14px solid transparent;
+                border-right: 14px solid transparent;
+                border-bottom: 20px solid white;
+                position: absolute;
+                right: 3%;
+                top: -2%;
+            }
+
+            h2{
+                font-size: 32px;
+                font-weight: bold;
+                color: #2F2926;
+                line-height: 120%;
+            }
+            .total-container{
                 display: flex;
-                flex-direction: row;
-                justify-content: space-around;
-                align-items: center;
-
-                h3{
-                    font-size: 23px;
-                    line-height: 120%;
-                    font-weight: bold;
-                }
-                span{
-                    width: 166px;
-                    height: 0px;
-                    border: 1px dashed rgba(0, 0, 0, 0.4);
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-start;
                     align-self: flex-end;
-                    margin: 0px 2px 4px 2px
+            
+                color: #2F2926;
+                .total{
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-around;
+                    align-items: center;
+
+                    h3{
+                        font-size: 23px;
+                        line-height: 120%;
+                        font-weight: bold;
+                    }
+                    span{
+                        width: 166px;
+                        height: 0px;
+                        border: 1px dashed rgba(0, 0, 0, 0.4);
+                        align-self: flex-end;
+                        margin: 0px 2px 4px 2px
+                    }
                 }
             }
+            > button{
+                width: 248px;
+                height: 55px;
+                background: #2F2926;
+                border-radius: 50px;
+                font-size: 24px;
+                font-weight: bold;
+                line-height: 120%;
+                color: white;
+                border: none;
+            }
         }
-        > button{
-            width: 248px;
-            height: 55px;
-            background: #2F2926;
-            border-radius: 50px;
-            font-size: 24px;
-            font-weight: bold;
-            line-height: 120%;
-            color: white;
-            border: none;
-            margin-bottom: 2rem;
+
+        .loader{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            
+
+
+            
+
+            img{
+                width: 100px;
+                height: 100px;
+                filter: invert(14%) sepia(1%) saturate(4489%) hue-rotate(334deg) brightness(96%) contrast(2%);
+                
+            }
+
         }
         
 
--- airbean/src/components/CartItem.vue
+++ airbean/src/components/CartItem.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="cart-item">
-      <div>
-          <h3>{{cart.title}}</h3>
-          <span></span>
-          <div>
-              <span class="arrow-up" @click="changeAmount('add')"></span>
-              <h3>{{cart.counter}}</h3>
-              <span class="arrow-down" @click="changeAmount('remove')"></span>
-          </div>
-      </div>
-      <p>{{cart.price}} kr</p>
+  <div class="cart-item"> 
+    <h3 class="cart-title">{{cart.title}}</h3>
+    <span class="cart-line"></span>
+    <div class="counter-container">
+        <span class="arrow-up" @click="changeAmount('add')"></span>
+        <h3>{{cart.counter}}</h3>
+        <span class="arrow-down" @click="changeAmount('remove')"></span>
+    </div>
+
+    <p>{{cart.price}} kr</p>
   </div>
 </template>
 
@@ -21,7 +20,6 @@ export default {
     methods: {
         changeAmount(choice) {
             let arr = [choice, this.cart.id]
-            console.log(arr)
            this.$store.commit("changeAmount", arr)
         }
     }
@@ -29,64 +27,72 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/setup/_normalize';
 .cart-item {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    margin-bottom: 10px;
+    width: 90vw;
+    height: 7vh;
+    margin-bottom: 5px;
+    display: grid;
+    grid-template-columns: max-content auto 15%;
+    grid-template-rows: 60% 40%;
+    padding-left: 10px;
+    padding-right: 10px;
 
     > p{
-        margin-top: -8px;
         font-size: 12px;
         font-weight: 400;
         color: #2F2926;
+        line-height: 15.6px;
+        justify-self: flex-start;
     }
 
-    > div{
+    .cart-title {
+        font-size: 20px;
+        color: #2F2926;
+        justify-self: flex-start;
+        line-height: 24px;
+        z-index: 2;
+        align-self: flex-end;
+        
+    }
+    .cart-line {
+            height: 0px;
+            border: 0.5px dashed rgba(0, 0, 0, 0.4);
+            margin-bottom: 2px;
+            margin-left: 0px;
+            margin-right: 0px;
+            z-index: 1;
+            align-self: flex-end;
+            
+    }
+    .counter-container {
+        grid-column: 3/4;
+        grid-row: 1/3;
         display: flex;
-        flex-direction: row;  
+        flex-direction: column;
         justify-content: space-evenly;
         align-items: center;
-        width: 20rem;
 
-        > h3 {
-            font-size: 20px;
-            color: #2F2926
+        .arrow-up {
+            width: 0px;
+            height: 0px;
+            border-left: 7px solid transparent;
+            border-right: 7px solid transparent;
+            border-bottom: 7px solid #2F2926;
         }
-        > span{
-                width: 166px;
-                height: 0px;
-                border: 0.5px dashed rgba(0, 0, 0, 0.4);
-                margin: 17px 5px 0px 5px;
-            }
-        > div {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-evenly;
-            align-items: center;
-            justify-self: flex-end;
-
-            .arrow-up {
-               width: 0px;
-                height: 0px;
-                border-left: 7px solid transparent;
-                border-right: 7px solid transparent;
-                border-bottom: 7px solid #2F2926;
-        } 
-        .arrow-down {
-                width: 0px;
-                height: 0px;
-                border-left: 7px solid transparent;
-                border-right: 7px solid transparent;
-                border-top: 7px solid #2F2926;
-        }
-            > h3 {
-                color: #2F2926;
-                font-size: 14px;
-                font-weight: 600;
-            }
-        }
+    } 
+    .arrow-down {
+            width: 0px;
+            height: 0px;
+            border-left: 7px solid transparent;
+            border-right: 7px solid transparent;
+            border-top: 7px solid #2F2926;
+    }
+    > h3 {
+        color: #2F2926;
+        font-size: 14px;
+        font-weight: 600;
     }
 }
+
 </style>
